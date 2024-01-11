8136cd7dabaaf48d2fce6ca3c1d2736a6e29cf0b
Simon Busley <simon.busley@gmail.com>
2024-01-11 16:54:51 +0100

    logging messages to be sent out

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,18 @@ fn main() {
     // Match participants
     let matching_round = match_participants(&participants_data);
 
-    // Print matches
+    // Print messages
+    println!("\n");
     for match_pair in &matching_round.matches {
+        let giver_first_name = &match_pair.giver.first_name;
+        let receiver_full_name = &match_pair.receiver.full_name();
         println!(
-            "Giver: {} - Receiver: {}",
-            match_pair.giver.name, match_pair.receiver.name
+            "Hi {giver_first_name} 😊 Dein Feedbackempfänger für die nächsten zwei Wochen ist {receiver_full_name}. \n
+Deine Aufgabe ist es, die nächsten zwei Wochen etwas auf ihn zu achten und ihm am Ende dieser zwei Wochen Feedback zu geben. \
+Das Feedback sollte im Idealfall so Sachen wie das Verhalten in und außerhalb von Meetings, Verhalten im Team, Code, Eigeninitiative etc. enthalten. \
+Mache am Ende der zwei Wochen bitte selber einen Termin mit ihm aus um ihm das Feedback zu geben. Er selber weiß ja nicht, wer ihm das Feedback geben wird. \n
+Viel Spaß 😊
+---"
         );
     }
 
