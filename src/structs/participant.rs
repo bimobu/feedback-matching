b1f31170b4dba346a9a7b34fbbbeb660fd36430d
Simon Busley <busley.simon@territory.group>
2024-01-11 16:54:51 +0100

    logging messages to be sent out

--- src/structs/participant.rs
+++ src/structs/participant.rs
@@ -3,5 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, Hash)]
 pub struct Participant {
     pub id: u32,
-    pub name: String,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+impl Participant {
+    pub fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
