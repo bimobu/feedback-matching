914a49bcc4b8ed28e40ddcf3cea77a08f1894d79
Simon Busley <simon.busley@gmail.com>
2024-01-25 16:19:42 +0100

    edited message

--- src/messages.rs
+++ src/messages.rs
@@ -23,11 +23,13 @@ pub fn print_messages_for_round(matching_round: &MatchingRound) {
             Gender::Female => "sie",
         };
         println!(
-          "Hi {giver_first_name} 😊 Dein Feedbackempfänger für die nächsten zwei Wochen ist {receiver_full_name}. \n
+"Hi {giver_first_name} 😊 Dein Feedbackempfänger für die nächsten zwei Wochen ist {receiver_full_name}. \
+\n
 Deine Aufgabe ist es, die nächsten zwei Wochen etwas auf {ihn_sie} zu achten und {ihm_ihr} am Ende dieser zwei Wochen Feedback zu geben. \
 Das Feedback sollte im Idealfall so Sachen wie das Verhalten in und außerhalb von Meetings, Verhalten im Team, Code, Eigeninitiative etc. enthalten. \
 Mache am Ende der zwei Wochen bitte selber einen Termin mit {ihm_ihr} aus um {ihm_ihr} das Feedback zu geben. {er_sie_capitalized} selber weiß ja nicht, wer {ihm_ihr} das Feedback geben wird. \
-Deswegen solltest du den Termin jetzt noch nicht erstellen. Sonst weiß {er_sie}, wer {ihm_ihr} Feedback geben wird, was das Ergebnis verfälschen könnte. \n
+Deswegen solltest du den Termin jetzt noch nicht erstellen. Sonst weiß {er_sie}, wer {ihm_ihr} Feedback geben wird, was das Ergebnis verfälschen könnte. \
+\n
 Viel Spaß 😊
 ---"
       );
