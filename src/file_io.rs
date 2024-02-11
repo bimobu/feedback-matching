b4f5e193318721815a6b5f86ef48f5fe25181554
Simon Busley <busley.simon@territory.group>
2024-02-11 15:53:17 +0100

    added JSON Schema

--- src/file_io.rs
+++ src/file_io.rs
@@ -1,20 +1,60 @@
 use crate::structs::matching_round::MatchingRound;
+use crate::structs::participants_file::ParticipantsFile;
 
+use jsonschema::JSONSchema;
 use serde::de::DeserializeOwned;
-use serde_json::from_reader;
+use serde_json::{from_reader, Value};
 use std::fs::{File, OpenOptions};
 use std::io::{Read, Seek, SeekFrom};
 
-pub fn read<T>(file_path: &str) -> T
+pub fn read_participants(file_path: &str) -> ParticipantsFile {
+    read::<ParticipantsFile>(file_path, "./data/schema/participants_schema.json")
+}
+
+pub fn read_matching_rounds(file_path: &str) -> Vec<MatchingRound> {
+    read::<Vec<MatchingRound>>(file_path, "./data/schema/matches_schema.json")
+}
+
+fn read<T>(file_path: &str, schema_path: &str) -> T
 where
     T: DeserializeOwned,
 {
+    let data_string = read_string(file_path);
+    let schema_string = read_string(schema_path);
+
+    let schema: Value =
+        serde_json::from_str(schema_string.as_str()).expect("Failed to parse JSON Schema");
+    let json_data: Value =
+        serde_json::from_str(data_string.as_str()).expect("Failed to parse JSON data");
+
+    let compiled_schema = JSONSchema::compile(&schema).expect("Failed to compile JSON Schema");
+    let validation_result = compiled_schema.validate(&json_data);
+
+    if let Err(errors) = validation_result {
+        println!("\n### JSON Validation Errors ###\n");
+
+        for (i, error) in errors.enumerate() {
+            println!(
+                "{}.Validation error: {}\nInstance path: {}\n",
+                i + 1,
+                error,
+                error.instance_path
+            );
+        }
+
+        print!("###\n\n")
+    }
+
+    from_reader(data_string.as_bytes()).expect("Failed to parse JSON")
+}
+
+fn read_string(file_path: &str) -> String {
     let mut file = File::open(file_path).expect("Failed to open JSON file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
         .expect("Failed to read JSON file");
 
-    from_reader(contents.as_bytes()).expect("Failed to parse JSON")
+    contents
 }
 
 pub fn write_matches(file_path: &str, round: MatchingRound) {
